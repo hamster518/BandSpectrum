4a89ab6dacd66a07c53fd107413977bf3a0d2ba8
Larry_LIu <hamster518@gmail.com>
2017-11-06 12:11:25 +0800

    Create web page

---
+++ BandSearch/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BandsearchConfig(AppConfig):
+    name = 'BandSearch'
--- Django_AsusWeb/settings.py
+++ Django_AsusWeb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'BandSearch',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
